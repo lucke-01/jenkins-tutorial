d2355dc5cddb904063eb35f1db32e797bf119373
Ricardo <Ricardo.Jara.Minano@smart4aviation.aero>
2024-07-24 11:40:32 +0200

    updated dsl v1

--- jenkins_dsl.groovy
+++ jenkins_dsl.groovy
@@ -1,7 +1,17 @@
 job('jenkins DSL Job') {
     description("")
-    scm {
-        git("https://github.com/lucke-01/jenkins-tutorial.git", master)
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url("https://github.com/lucke-01/jenkins-tutorial.git")
+                    }
+                    branch('*/master')
+                }
+            }
+            lightweight()
+        }
     }
     triggers {
         scm('*/5 * * * *')
